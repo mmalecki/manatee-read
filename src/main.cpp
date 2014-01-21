ce513e1ab0cd70c061622831537510f68a13b604
Maciej Małecki <me@mmalecki.com>
2014-01-21 23:47:10 +0100

    WOOOOO!

--- src/main.cpp
+++ src/main.cpp
@@ -19,10 +19,15 @@ int main(int argc,char **argv) {
   image.write(&raw, "GRAY", 8);
   const void *data = raw.data();
 
-  cout << "Read image: " << height << " x " << width << endl;
+  cout << "Read image: " << height << " x " << width << " (" << raw.length() << ")" << endl;
 
   MWB_setActiveCodes(MWB_CODE_MASK_PDF);
   MWB_setDirection(MWB_SCANDIRECTION_HORIZONTAL | MWB_SCANDIRECTION_VERTICAL);
   MWB_setLevel(5);
+
+  uint8_t* decoded;
+  int i = MWB_scanGrayscaleImage((uint8_t*) data, width, height, &decoded);
+  cout << "Manatee returned " << i << endl;
+  cout << (char*) decoded;
   return 0;
 }
